151e115583fd34cb5dd22eb93d47013f2955fa9b
KARINA SOUZA HONORATO DO NASCIMENTO <25162417@edu119.local>
2026-08-05 16:57:28 -0300

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorSchemeSeed: Colors.blue, useMaterial3: true),
+      home: MediaEscolarPage(),
+    );
+  }
+}
+
+class MediaEscolarPage extends StatefulWidget {
+  const MediaEscolarPage({super.key});
+
+  @override
+  State<MediaEscolarPage> createState() => _MediaEscolarPageState();
+}
+
+class _MediaEscolarPageState extends State<MediaEscolarPage> {
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController nota1Controller = TextEditingController();
+  final TextEditingController nota2Controller = TextEditingController();
+  final TextEditingController nota3Controller = TextEditingController();
+
+  String nomeAluno = '';
+  String situacao = '';
+  double media = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calculadora de Média"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Icon(Icons.school, size: 80),
+
+            const SizedBox(height: 10),
+
+            const Text(
+              'Média Escolar',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+                ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
